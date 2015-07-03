c75965ca93abc47edcebc8390cfcf3679bb8784c
jexenberger <jexenberger@gmail.com>
2015-07-03 17:05:34 +0200

    removed silly variable names

--- ScalaEuler/src/SumMultiple3And5.scala
+++ ScalaEuler/src/SumMultiple3And5.scala
@@ -1,8 +1,8 @@
 object SumMultiple3And5 {
-  def ->(x: (Int) => Unit) = x((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
+  def ->(function: (Int) => Unit) = function((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
 
   def main(args: Array[String]) {
-    -> (x => println(x))
+    -> (i => println(i))
   }
 }
 

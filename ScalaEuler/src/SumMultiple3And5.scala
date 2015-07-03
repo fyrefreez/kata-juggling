902c91867b878ccfd62a6b1df59547ffdc4e0be4
jexenberger <jexenberger@gmail.com>
2015-07-03 17:16:38 +0200

    added "test"

--- ScalaEuler/src/SumMultiple3And5.scala
+++ ScalaEuler/src/SumMultiple3And5.scala
@@ -1,7 +1,7 @@
 object SumMultiple3And5 {
   def ->(function: (Int) => Unit) = function((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
 
-  def main(args: Array[String]) = -> (i => println(i))
+  def main(args: Array[String]) = -> (i => Predef.assert(i==233168))
 }
 
 

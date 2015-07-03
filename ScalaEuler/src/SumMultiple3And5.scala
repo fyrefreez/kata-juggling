a9f5c8b4c15fa6a0e5f136c8d686e07a98b7d709
jexenberger <jexenberger@gmail.com>
2015-07-03 16:58:19 +0200

    le bug

--- ScalaEuler/src/SumMultiple3And5.scala
+++ ScalaEuler/src/SumMultiple3And5.scala
@@ -1,5 +1,5 @@
 object SumMultiple3And5 {
-  def ->(x: (Int) => Unit) = println((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
+  def ->(x: (Int) => Unit) = x((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
 
   def main(args: Array[String]) {
     -> (x => println(x))

e159512e3b017c689f891d9421bee7a939d15095
jexenberger <jexenberger@gmail.com>
2015-07-03 16:56:34 +0200

    Added Scala module as well as first problem

---
+++ ScalaEuler/src/SumMultiple3And5.scala
@@ -0,0 +1,9 @@
+object SumMultiple3And5 {
+  def ->(x: (Int) => Unit) = println((for (i<-1 until 1000 if i%3==0|i%5==0) yield i).sum)
+
+  def main(args: Array[String]) {
+    -> (x => println(x))
+  }
+}
+
+

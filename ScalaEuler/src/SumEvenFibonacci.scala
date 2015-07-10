d834e9ef546cce9ea8a683eb63448c7ebcc4d8bd
jexenberger <jexenberger@gmail.com>
2015-07-10 14:30:06 +0200

    added Even Fibonacci Sum

--- ScalaEuler/src/SumEvenFibonacci.scala
+++ ScalaEuler/src/SumEvenFibonacci.scala
@@ -1,9 +1,9 @@
 object SumEvenFibonacci {
   val fibs: Stream[BigInt] = BigInt(1) #:: BigInt(2) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2}
-  val expectedFibs = List(2, 8, 34, 144, 610, 2584, 10946, 46368, 196418, 832040, 3524578).sum
+  val expectedFibs = List(2, 8, 34, 144, 610, 2584, 10946, 46368, 196418, 832040, 3524578) sum
 
   def evenTerms(max: Long) = {
-    fibs.takeWhile(_ <= max).filter(_ % 2 == 0)
+    fibs takeWhile(_ <= max) filter(_ % 2 == 0)
   }
 
 
